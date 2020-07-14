68467b1de3f5849d4da1772da0fa0a4c09326925
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-14 19:10:32 +0700

    Using Animated API for complex animations

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import Module from './src/layoutAnimationAdvanced';
+import Module from './src/animated';
 
 export default function App() {
   return <Module />;
