26491776da9e19859be39098488b0a2942d6ce67
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-14 19:25:36 +0700

    ...

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import Module from './src/animated';
+import Module from './src/animatedComplex';
 
 export default function App() {
   return <Module />;
